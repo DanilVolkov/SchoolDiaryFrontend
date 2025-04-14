cb3dedc3c14ad29a55e855f1b0bb7bc6892711cb
ActiWait <trosio2003@gmail.com>
2025-04-14 17:18:01 +0300

    Закончены переименования

--- SchoolDiary/Properties/Resources.Designer.cs
+++ SchoolDiary/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SchoolDiary_wpf.Properties {
+namespace SchoolDiary.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SchoolDiary_wpf.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SchoolDiary_wpf.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SchoolDiary.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
