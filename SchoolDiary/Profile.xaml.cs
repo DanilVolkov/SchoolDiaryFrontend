e1181a5e881f8d31b123f0fe5086d3fb5b700e8c
Юрий Шальнов <jra125.opr103@gmail.com>
2025-04-14 23:54:24 +0300

    Добавил прозрачность менюшки в расписании на неделю и день, А также починил кнопку меню для екзешника

--- SchoolDiary/Profile.xaml.cs
+++ SchoolDiary/Profile.xaml.cs
@@ -89,21 +89,18 @@ namespace SchoolDiary_wpf
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            // Действие при клике
             if (Menu.Width == 216)
             {
                 Menu.Width = 64;
                 Schedule.Width = 48;
                 Grade.Width = 48;
 
-                //MainSheduleField.Margin = new Thickness(112, 152, 0, 0);  //сделал эти трансформации отступов для сдвига при нажатии расширения кнопок меню, но судя по всему, по дизайну фигмы оно не нужно
-                //TimePeriodPanel.Margin = new Thickness(64, 64, 0, 0);
+                // Изменяем изображения
+                ((ImageBrush)Schedule.Background).ImageSource = new BitmapImage(
+                    new Uri("pack://application:,,,/ImageButtons/button_manu_close_schedule_default.png", UriKind.Absolute));
 
-                ((ImageBrush)Schedule.Background).ImageSource = new System.Windows.Media.Imaging.BitmapImage(
-            new System.Uri("../../ImageButtons/button_manu_close_schedule_default.png", System.UriKind.Relative));
-                ((ImageBrush)Grade.Background).ImageSource = new System.Windows.Media.Imaging.BitmapImage(
-                    new System.Uri("../../ImageButtons/button_menu_close_mark_defoult.png", System.UriKind.Relative));
-
+                ((ImageBrush)Grade.Background).ImageSource = new BitmapImage(
+                    new Uri("pack://application:,,,/ImageButtons/button_menu_close_mark_defoult.png", UriKind.Absolute));
             }
             else
             {
@@ -111,14 +108,12 @@ namespace SchoolDiary_wpf
                 Schedule.Width = 184;
                 Grade.Width = 184;
 
-                //MainSheduleField.Margin = new Thickness(264, 152, 0, 0);
-                //TimePeriodPanel.Margin = new Thickness(216, 64, 0, 0);
+                // Возвращаем исходные изображения
+                ((ImageBrush)Schedule.Background).ImageSource = new BitmapImage(
+                    new Uri("pack://application:,,,/ImageButtons/button_menu_schedule_default.png", UriKind.Absolute));
 
-                // Возвращаем исходные фоновые изображения кнопок
-                ((ImageBrush)Schedule.Background).ImageSource = new System.Windows.Media.Imaging.BitmapImage(
-                    new System.Uri("../../ImageButtons/button_menu_schedule_default.png", System.UriKind.Relative));
-                ((ImageBrush)Grade.Background).ImageSource = new System.Windows.Media.Imaging.BitmapImage(
-                    new System.Uri("../../ImageButtons/button_menu_mark_default.png", System.UriKind.Relative));
+                ((ImageBrush)Grade.Background).ImageSource = new BitmapImage(
+                    new Uri("pack://application:,,,/ImageButtons/button_menu_mark_default.png", UriKind.Absolute));
             }
         }
 
