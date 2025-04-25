7df4e9616d086a809fdbcac21a812cecd66d0631
ActiWait <trosio2003@gmail.com>
2025-04-25 17:47:11 +0300

    Изменен namespace на Models

--- SchoolDiary/Models/Group.cs
+++ SchoolDiary/Models/Group.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace SchoolDiary.Objects
+namespace SchoolDiary.Models
 {
     public class Group
     {
---
+++ SchoolDiary/Models/SubjectMarks.cs
@@ -0,0 +1,18 @@
+﻿using SchoolDiary.Models;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace SchoolDiary.Models
+{
+    class SubjectMarks
+    {
+        [JsonPropertyName("subject")]
+        public Subject Subject { get; set; }
+
+        [JsonPropertyName("marks")]
+        public List<Mark> Marks { get; set; }
+
+        [JsonPropertyName("average")]
+        public double? Average { get; set; }
+    }
+}
