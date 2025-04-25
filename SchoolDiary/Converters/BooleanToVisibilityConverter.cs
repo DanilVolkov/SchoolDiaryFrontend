c50bed59a49ef292ffeabf3383aa9b8704e15194
Юрий Шальнов <jra125.opr103@gmail.com>
2025-04-26 02:07:45 +0300

    refacroring_code-min

---
+++ SchoolDiary/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace SchoolDiary.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Models.Homework hw)
+            {
+                // Если строка не пустая, возвращаем Visibility.Visible
+                return !string.IsNullOrEmpty(hw.Description) ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
