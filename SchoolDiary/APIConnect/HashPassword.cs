03ce37c1a4c5da083821cc6329443bc13b7ee542
ActiWait <trosio2003@gmail.com>
2025-04-25 17:50:02 +0300

    Добавлен класс для вычсиления хэша паролей

---
+++ SchoolDiary/APIConnect/HashPassword.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDiary.APIConnect
+{
+    class HashPassword
+    {
+        public static string GetHash(string password)
+        {
+            using (var pbkdf2 = new Rfc2898DeriveBytes(password, Encoding.UTF8.GetBytes(Properties.Resources.salt), 1000))
+            {
+                byte[] hash = pbkdf2.GetBytes(20);
+                return BitConverter.ToString(hash).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
