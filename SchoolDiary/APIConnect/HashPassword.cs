dff34b43978772d902ffe9d512afaf72686fe105
ActiWait <trosio2003@gmail.com>
2025-04-26 09:57:15 +0300

    Обнолвена работа с salt для хэша паролей

--- SchoolDiary/APIConnect/HashPassword.cs
+++ SchoolDiary/APIConnect/HashPassword.cs
@@ -8,8 +8,8 @@ namespace SchoolDiary.APIConnect
     {
         public static string GetHash(string password)
         {
-            var v = Environment.GetEnvironmentVariable("salt", EnvironmentVariableTarget.User);
-            using (var pbkdf2 = new Rfc2898DeriveBytes(password, Encoding.UTF8.GetBytes(v), 1000))
+            //var v = Environment.GetEnvironmentVariable("salt", EnvironmentVariableTarget.User);
+            using (var pbkdf2 = new Rfc2898DeriveBytes(password, Encoding.UTF8.GetBytes(password), 1000))
             {
                 byte[] hash = pbkdf2.GetBytes(20);
                 return BitConverter.ToString(hash).Replace("-", "").ToLower();
